8be9ebe5a0097db94809338bc8b130dea6628fe4
eNKay <nguyenkhai24082408@gmail.com>
2024-06-16 03:40:24 +0700

    Add server.js for PRJ2

---
+++ PRJ2-IssueTracker/server.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const expect = require("chai").expect;
+const cors = require("cors");
+require("dotenv").config();
+
+const apiRoutes = require("./routes/api.js");
+
+let app = express();
+
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.use(cors({ origin: "*" })); //For FCC testing purposes only
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Sample front-end
+app.route("/:project/").get(function (req, res) {
+	res.sendFile(process.cwd() + "/views/issue.html");
+});
+
+//Index page (static HTML)
+app.route("/").get(function (req, res) {
+	res.sendFile(process.cwd() + "/views/index.html");
+});
+
+//Routing for API
+apiRoutes(app);
+
+//404 Not Found Middleware
+app.use(function (req, res, next) {
+	res.status(404).type("text").send("Not Found");
+});
+
+//Start our server and tests!
+const server = app.listen(process.env.PORT || 3000, function () {
+	console.log("Your app is listening on port " + server.address().port);
+});
+
+module.exports = server; //for testing
