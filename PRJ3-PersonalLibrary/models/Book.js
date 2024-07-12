7868bcfaeaa5120b0215de7b1b2d9bf47bac92de
eNKay <nguyenkhai24082408@gmail.com>
2024-07-12 12:09:42 +0700

    Add models for PRJ3

---
+++ PRJ3-PersonalLibrary/models/Book.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+	title: { type: String, required: true },
+	comments: { type: [String], default: [] },
+	commentcount: { type: Number, default: 0 },
+});
+
+module.exports = mongoose.model("Book", bookSchema);
