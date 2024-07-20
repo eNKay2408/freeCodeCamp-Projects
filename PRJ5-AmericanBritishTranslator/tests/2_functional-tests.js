39627bc35b79ff971ba02425bf055556e3afe280
eNKay <nguyenkhai24082408@gmail.com>
2024-07-20 23:21:23 +0700

    Add functional-tests for PRJ5

---
+++ PRJ5-AmericanBritishTranslator/tests/2_functional-tests.js
@@ -0,0 +1,101 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const assert = chai.assert;
+const server = require("../server.js");
+
+chai.use(chaiHttp);
+
+describe("Functional Tests", () => {
+	describe("POST request to /api/translate", () => {
+		it("Translation with text and locale fields", (done) => {
+			chai
+				.request(server)
+				.post("/api/translate")
+				.send({
+					text: "Mangoes are my favorite fruit.",
+					locale: "american-to-british",
+				})
+				.end((err, res) => {
+					assert.equal(res.status, 200);
+					assert.equal(
+						res.body.translation,
+						'Mangoes are my <span class="highlight">favourite</span> fruit.'
+					);
+					done();
+				});
+		});
+
+		it("Translation with text and invalid locale field", (done) => {
+			chai
+				.request(server)
+				.post("/api/translate")
+				.send({
+					text: "Mangoes are my favorite fruit.",
+					locale: "invalid-locale",
+				})
+				.end((err, res) => {
+					assert.equal(res.status, 200);
+					assert.equal(res.body.error, "Invalid value for locale field");
+					done();
+				});
+		});
+
+		it("Translation with missing text field", (done) => {
+			chai
+				.request(server)
+				.post("/api/translate")
+				.send({
+					locale: "american-to-british",
+				})
+				.end((err, res) => {
+					assert.equal(res.status, 200);
+					assert.equal(res.body.error, "Required field(s) missing");
+					done();
+				});
+		});
+
+		it("Translation with missing locale field", (done) => {
+			chai
+				.request(server)
+				.post("/api/translate")
+				.send({
+					text: "Mangoes are my favorite fruit.",
+				})
+				.end((err, res) => {
+					assert.equal(res.status, 200);
+					assert.equal(res.body.error, "Required field(s) missing");
+					done();
+				});
+		});
+
+		it("Translation with empty text", (done) => {
+			chai
+				.request(server)
+				.post("/api/translate")
+				.send({
+					text: "",
+					locale: "american-to-british",
+				})
+				.end((err, res) => {
+					assert.equal(res.status, 200);
+					assert.equal(res.body.error, "No text to translate");
+					done();
+				});
+		});
+
+		it("Translation with text that needs no translation", (done) => {
+			chai
+				.request(server)
+				.post("/api/translate")
+				.send({
+					text: "Mangoes are my favourite fruit.",
+					locale: "american-to-british",
+				})
+				.end((err, res) => {
+					assert.equal(res.status, 200);
+					assert.equal(res.body.translation, "Everything looks good to me!");
+					done();
+				});
+		});
+	});
+});
